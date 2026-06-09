222f5fecef536c0b2330073a87fda59f0bab2b77
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-06-09 22:44:10 +0545

    ball script added, ballBouncyness physics material also added & Ball angle (while hitting playerpaddle) fucntion added. now turn to make the bricks alive

--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -5,7 +5,8 @@ public class PlayerPaddle : MonoBehaviour
 {
     public Vector2 direction { get; private set;}
     public new Rigidbody2D rigidbody { get; private set; }
-    public float speed = 30f;      
+    public float speed = 30f;  
+    public float MaxBallBounceAngles = 75f;    
     private void Awake()
     {
         this.rigidbody = GetComponent<Rigidbody2D>();
@@ -38,6 +39,27 @@ public class PlayerPaddle : MonoBehaviour
         {
             this.rigidbody.AddForce(this.direction * this.speed);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if(ball != null)
+        {
+            Vector3 PaddlePosition = this.transform.position;
+            Vector2 ContactPoint = collision.GetContact(0).point;
+
+            float Offset = PaddlePosition.x - ContactPoint.x;
+            float Width = collision.otherCollider.bounds.size.x / 2;
+            float CurrentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.linearVelocity);
+            float BounceAngle = (Offset / Width) * this.MaxBallBounceAngles;
+            float UpgradeAngle =  Mathf.Clamp(CurrentAngle + BounceAngle, -this.MaxBallBounceAngles, this.MaxBallBounceAngles);
+
+            Quaternion Rotation = Quaternion.AngleAxis(UpgradeAngle, Vector3.forward);
+            
+            ball.rigidbody.linearVelocity = Rotation * Vector2.up * ball.rigidbody.linearVelocity.magnitude;
+        } 
     }
 
 }
