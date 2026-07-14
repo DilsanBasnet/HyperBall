c8e00dd5d548aa498d3d633294df0794f8079b7a
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-07-14 14:05:04 +0545

    script added for gameover scene and also added the logic for buttons ...

--- Assets/Scripts/gameovercontroller.cs
+++ Assets/Scripts/gameovercontroller.cs
@@ -1,1 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameovercontroller : MonoBehaviour
+{
+   public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        
+            }
+
+            public void ExitGame()
+    {
+        Debug.Log("Game Exits...");
+        Application.Quit();
+        
+    }
+}
